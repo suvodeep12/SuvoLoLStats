5d4c3e9566cbae5a65a7648b0fc55014f346f3da
Suvodeep Ghosh <suvodeepg@gmail.com>
2023-02-08 13:37:58 +0530

    Trying match history.

--- lolapi.py
+++ lolapi.py
@@ -9,6 +9,8 @@ api = os.getenv('API_KEY')
 print(api)
 watcher = LolWatcher(api)
 my_region = 'sg2'
+if my_region == 'sg2':
+    my_area = 'sea'
 
 #Define my profile
 me = watcher.summoner.by_name(my_region, 'Suvo')
@@ -16,7 +18,7 @@ me = watcher.summoner.by_name(my_region, 'Suvo')
 
 #Get ranked stats
 my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
-print(my_ranked_stats)
+# print(my_ranked_stats)
 
 #Get latest version
 latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
@@ -37,3 +39,15 @@ for key in static_champ_list['data'].keys():
         champ_dict[static_champ_list['data'][key]['key']] = key
 
 # print(champ_dict)
+
+my_matches = watcher.match.matchlist_by_puuid(my_area, me['puuid'])
+
+# fetch last match detail
+last_match = my_matches[0]
+
+match_detail = watcher.match.by_id(my_area, last_match)
+
+participants = []
+print(match_detail['metadata']['participants'])
+for row in match_detail['metadata']['participants']:
+    participants_row = {}
