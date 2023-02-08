12c452db230a5d2bb83ecf4d1daa1e1fc7689732
Suvodeep Ghosh <suvodeepg@gmail.com>
2023-02-08 15:23:28 +0530

    Added last match details.

--- lolapi.py
+++ lolapi.py
@@ -48,6 +48,25 @@ last_match = my_matches[0]
 match_detail = watcher.match.by_id(my_area, last_match)
 
 participants = []
-print(match_detail['metadata']['participants'])
-for row in match_detail['metadata']['participants']:
+# print(match_detail['info']['participants'])
+for row in match_detail['info']['participants']:
     participants_row = {}
+    participants_row['champion'] = row['championId']
+    participants_row['summonerspell1'] = row['summoner1Id']
+    participants_row['summonerspell2'] = row['summoner2Id']
+    participants_row['win'] = row['win']
+    participants_row['kills'] = row['kills']
+    participants_row['deaths'] = row['deaths']
+    participants_row['assists'] = row['assists']
+    participants_row['totalDamageDealt'] = row['totalDamageDealt']
+    participants_row['goldEarned'] = row['goldEarned']
+    participants_row['champLevel'] = row['champLevel']
+    participants_row['totalMinionsKilled'] = row['totalMinionsKilled']
+    participants_row['item0'] = row['item0']
+    participants_row['item1'] = row['item1']
+    participants.append(participants_row)
+
+# print(participants)
+
+df = pd.DataFrame(participants)
+print(df)
